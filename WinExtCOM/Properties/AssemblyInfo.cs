365a452aee069667c8cb60edef4250978a410cfc
yury deshin <j.deshin@hotmail.com>
2018-12-01 18:02:59 +0300

    Добавил модуль обработки

--- WinExtCOM/Properties/AssemblyInfo.cs
+++ WinExtCOM/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a217ebb6-76d6-4192-a9d7-5feb2b8060f6")]
